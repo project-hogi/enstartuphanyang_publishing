67eb46c071f66add3c5c583f500fa538cf74327e
조용준 <junishop@gmail.com>
2016-08-19 02:42:25 +0900

    at home

--- eng_sub_2programs_4incubating_4.php
+++ eng_sub_2programs_4incubating_4.php
@@ -24,63 +24,63 @@
 					</li>
 				</ul>
 			</div><!-- //programs_tab -->
-			<div id="container">
-				<div id="incubating_club">
-					<h3>
-						Startup Club
-					</h3>
-					<p class="txt1">
-						Startup Club discovers and supports startup clubs that have fresh and original ideas, provide excellent club startup spaces, etc., and support production.
-					</p>
-					<ul class="club">
-						<li>
-							<dl>
-								<dt>
-									Purpose
-								</dt>
-								<dd>
-									ㆍDiscover and support college student startup clubs that have fresh and new ideas, create a positive atmosphere for startups, and promote youth startups
-								</dd>
-							</dl>
-						</li>
-						<li>
-							<dl>
-								<dt>
-									Eligibility
-								</dt>
-								<dd>
-									ㆍTeams with more than 3 members (including the representative of startup club) consisting of Hanyang University students and students on leave of absence, who have fresh and original ideas and have startup determination<br />
-									ㆍThe representative must be a Hanyang University student (including students on leave of absence) (can recruit other university students as additional members)
-								</dd>
-							</dl>
-						</li>
-						<li>
-							<dl>
-								<dt>
-									Selection Process
-								</dt>
-								<dd>
-									ㆍSubmit startup club application forms -> Document screening -> Interview evaluation
-								</dd>
-							</dl>
-						</li>
-						<li>
-							<dl>
-								<dt>
-									Support Details
-								</dt>
-								<dd>
-									ㆍProvide a common startup preparation space for the students of excellent startup clubs <br />
-									ㆍDifferentiated support of item development cost (prototype production support) <br />
-									ㆍSupport corporate foundation commission when establishing a company<br />
-									ㆍSupport expert mentoring and consulting <br />
-									ㆍSupport the acquisition of intellectual property right<br />
-									ㆍHelp connection with other startup support programs
-								</dd>
-							</dl>
-						</li>
-					</ul>
-				</div><!-- //incubating_club -->
+			<div id="container"><div class="conwrap">
+							<div id="incubating_club">
+								<h3>
+									Startup Club
+								</h3>
+								<p class="txt1">
+									Startup Club discovers and supports startup clubs that have fresh and original ideas, provide excellent club startup spaces, etc., and support production.
+								</p>
+								<ul class="club">
+									<li>
+										<dl>
+											<dt>
+												Purpose
+											</dt>
+											<dd>
+												ㆍDiscover and support college student startup clubs that have fresh and new ideas, create a positive atmosphere for startups, and promote youth startups
+											</dd>
+										</dl>
+									</li>
+									<li>
+										<dl>
+											<dt>
+												Eligibility
+											</dt>
+											<dd>
+												ㆍTeams with more than 3 members (including the representative of startup club) consisting of Hanyang University students and students on leave of absence, who have fresh and original ideas and have startup determination<br />
+												ㆍThe representative must be a Hanyang University student (including students on leave of absence) (can recruit other university students as additional members)
+											</dd>
+										</dl>
+									</li>
+									<li>
+										<dl>
+											<dt>
+												Selection Process
+											</dt>
+											<dd>
+												ㆍSubmit startup club application forms -> Document screening -> Interview evaluation
+											</dd>
+										</dl>
+									</li>
+									<li>
+										<dl>
+											<dt>
+												Support Details
+											</dt>
+											<dd>
+												ㆍProvide a common startup preparation space for the students of excellent startup clubs <br />
+												ㆍDifferentiated support of item development cost (prototype production support) <br />
+												ㆍSupport corporate foundation commission when establishing a company<br />
+												ㆍSupport expert mentoring and consulting <br />
+												ㆍSupport the acquisition of intellectual property right<br />
+												ㆍHelp connection with other startup support programs
+											</dd>
+										</dl>
+									</li>
+								</ul>
+							</div><!-- //incubating_club --></div>
 			</div><!-- //container -->
 		</div><!-- //containerwrap -->
 
--- inc/js.php
+++ inc/js.php
@@ -1,4 +1,7 @@
     <script src="js/jquery.js"></script>
     <script src="js/jquery-ui.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/534ab90430.js"></script>
+	<script src="js/jquery.cycle.js"></script>
     <script src="js/common.js"></script>
     <script src="js/login_popup.js"></script>
