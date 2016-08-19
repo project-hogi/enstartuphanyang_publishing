5f5b58119fb4ce9a36bb06d6e4a19852dadd724e
오준영 <icon1123@gmail.com>
2016-08-19 10:58:12 +0900

    1

--- inc/js.php
+++
@@ -1,6 +0,0 @@
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery-ui.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.cycle2.js"></script>
-    <script src="js/common.js"></script>
-    <script src="js/login_popup.js"></script>
---
+++ www/inc/header.php
@@ -0,0 +1,54 @@
+<div id="headerwrap">
+	<header id="header">
+		<h1><a href="index.php"><img src="img/logo.png" alt="HANYANG STARTUP LOUNGE" /></a></h1>
+		<nav id="nav">
+			<ul class="gnb">
+				<li>
+					<a href="eng_sub_1about_1.php">About</a>
+					<ul class="sgnb">
+						<li><a href="eng_sub_1about_1.php">Greeting</a></li>
+						<li><a href="eng_sub_1about_2.php">Startup Lounge</a></li>
+						<li><a href="eng_sub_1about_3.php">Center for Global Enterpreneurship</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="eng_sub_2programs_1education_1.php">Programs</a>
+					<ul class="sgnb">
+						<li><a href="eng_sub_2programs_1education_1.php">Education</a></li>
+						<li><a href="eng_sub_2programs_3training_1.php">Training</a></li>
+						<li><a href="eng_sub_2programs_2networking_1.php">Networking</a></li>
+						<li><a href="eng_sub_2programs_4incubating_1.php">Incubating</a></li> 
+					</ul>
+				</li>
+				<li>
+					<a href="eng_sub_3angelinvestmentfund_1.php">Angel Investment Fund</a>
+					<ul class="sgnb">
+						<li><a href="eng_sub_3angelinvestmentfund_1.php">Hanyang Angel Club</a></li>
+						<li><a href="eng_sub_3angelinvestmentfund_2.php">Hanyang Youth Startup Fund</a></li>
+						<li><a href="eng_sub_3angelinvestmentfund_3.php">Crowd Funding</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="eng_sub_4startupinfo_1.php">Startup Info</a>
+					<ul class="sgnb">
+						<li><a href="eng_sub_4startupinfo_1.php">Notice & News</a></li>
+						<li><a href="eng_sub_4startupinfo_2gallery.php">Gallery</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav><!-- //nav -->
+		<div id="language">
+			<p>LANGUAGE</p>
+			<ul>
+				<li><a href="http://startup.hanyang.ac.kr/" target="_blank">Korean</a></li>
+				<li><a href="/">English</a></li>
+				<li><a href="http://cnstartuphanyang.hogi.co.kr" target="_blank">Chinese</a></li>
+			</ul>
+		</div>
+		
+		
+	</header><!-- //header -->
+	<div id="menu">
+		<p><a href="#"><img src="img/menu_btn.png" alt="menu btn" /></a></p>
+	</div><!-- //menu -->
+</div><!-- //headerwrap -->
